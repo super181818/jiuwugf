14d543bbd94415506d6ece12eef3df0f9722813b
super181818 <131360198+super181818@users.noreply.github.com>
2024-06-05 11:59:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /*-------config start-------*/
-var _wip = 'yylx.panguh5.com';  
+var _wip = '111.223.12.77';
 var _port = '1708';  
 /*---------config end---------*/
 
@@ -7,6 +7,4 @@ var BaseUrl = 'http://' + _wip+':'+_port;
 var ResourceUrl = '/';
 var Suffix = '.html';
 var _isTx = 1;
-var _wport = '10000' ;
-BaseUrl = 'http://yyds.panguh5.com:1998/' + _port ;
-_wport = '1997' ;
+var _wport = '10000' 
